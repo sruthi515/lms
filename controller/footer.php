5006210f109d7109b1b9196392323c6c7ce79fdd
rohith <sairohith@msupply.com>
2017-10-15 19:45:06 +0530

    added files

---
+++ controller/footer.php
@@ -0,0 +1,17 @@
+
+<?php
+	
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<div class="footerContent">
+			Rise library @ 2017 | Contact Us
+		</div>
+	</body>
+</html>
---
+++ controller/navigation.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="../css/navigation.css">
+	</head>
+	<body>
+		<ul>
+			<!-- <li><a href="home.php?activity=dashboard">Home</a></li> -->
+			<li><a href="home.php?activity=search">Home</a></li>			
+			<li><a href="home.php?activity=register">Register</a></li>
+			<li><a href="home.php?activity=adminLogin">Admin</a></li>
+			<li><a href="home.php?activity=studentLogin">Student login</a></li>
+			<li><a href="home.php?activity=facultyLogin">Faculty login</a></li>
+			<li><a href="home.php?activity=dashboard">Dashboard</a></li>
+		</ul>
+	</body>
+</html>
